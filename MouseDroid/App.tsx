adfaaffc65495da5284cbc2720d052cf73857155
BrendenPerson <rodriguezb0616@gmail.com>
2023-11-25 22:14:58 -0600

    Network menu progress

--- MouseDroid/App.tsx
+++ MouseDroid/App.tsx
@@ -1,11 +1,13 @@
-import React, { Component } from 'react';
+import React, {useState, Component } from 'react';
 import type {PropsWithChildren} from 'react';
 import {
   Text,
   View,
   StyleSheet,
   Image,
-  Button
+  ImageBackground,
+  Button,
+  FlatList
 } from 'react-native';
 import { Navigation } from 'react-native-navigation';
 
@@ -39,6 +41,20 @@ function Logo()
   )
 }
 
+const ExpandableNetwork = ({ item }) => {
+  const [expanded, setExpanded] = useState(false);
+
+  const toggleExpand = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <View>
+
+    </View>
+  );
+};
+
 // maybe we make our own library file so we can share tags and stylesheets between screens
 
 /* ^ defining your own tag?
@@ -63,10 +79,99 @@ if first
 Navigation.registerComponent('com.MouseDroid.name_of_screen', () => name_of_screen)
 fi
 */
+const NetworkList = ({ data }) => {
 
+  return (
+    <View style={{ alignItems: 'center', paddingTop: 25, height: 450}}>
+    
+      <FlatList 
+        data={data} 
+        renderItem={({ item }) => (
+          <View style={{ paddingBottom: 25 }}>
+            <View style={styles.nextworkButton}>
+              <Text style={styles.listText}> {item.name} </Text>
+            </View>
+          </View>
+        )} 
+        keyExtractor={(item) => item.id.toString()} 
+      /> 
+
+    </View>
+  );
+}
 
 const WelcomeScreen = (props) => {
+  // dummy data
+  const [networks, setNetwork] = useState([
+    {id: '1', name: 'MouseDroid_1', strength: '3'},
+    {id: '2', name: 'MouseDroid_2', strength: '4' },
+    {id: '3', name: 'MouseDroid_3', strength: '1' },
+    {id: '4', name: 'MouseDroid_1', strength: '3'},
+    {id: '5', name: 'MouseDroid_2', strength: '4' },
+    {id: '6', name: 'MouseDroid_3', strength: '1' },
+    {id: '7', name: 'MouseDroid_1', strength: '3'},
+    {id: '8', name: 'MouseDroid_2', strength: '4' },
+    {id: '9', name: 'MouseDroid_3', strength: '1' },
+  ]);
+
   return (
+    <View style={styles.container}>
+      <ImageBackground 
+        source={require('./images/Background.png')} style={styles.Background} resizeMode="repeat"
+      />
+
+      <View style={styles.header}>
+        <View style={styles.pad}>
+          <Image
+            style={styles.icon}
+            resizeMethod='resize'
+            source={require('./images/MouseDroidIcon.png')} 
+          />
+        </View>
+
+        <Text style={styles.boldText}>
+          Mouse WiFi {'\n'}Booster
+        </Text> 
+      </View>
+
+
+      <View style={styles.listArea}>
+        <View style={{ paddingTop: 30, alignItems: 'center' }}>
+
+          <Text style={{
+              height: 50,
+              fontSize: 30,
+              color:'#000'
+            }}>
+            Select a Booster
+          </Text> 
+
+          <View style={{
+            width: 300,
+            height: 4,
+            borderRadius: 50,
+            backgroundColor:'#000',
+          }}></View>
+        </View>
+
+        <NetworkList  data={networks} />
+
+        <View style={{ paddingLeft: 100 }}>
+          <Section>
+            <Button title="Switch to second screen"
+            onPress={() => Navigation.push(props.componentId, {
+              component:{
+                name:"TestScreen",
+              }
+            })}/>
+          </Section>
+        </View>
+      </View>
+    </View>
+  ); 
+
+
+  /*return (
         <View>
           <Logo/>
           <Section title="New Section">
@@ -79,7 +184,7 @@ const WelcomeScreen = (props) => {
             })}/>
           </Section>
         </View>
-  );
+  ); */
 }
 WelcomeScreen.options = {
   topBar:{
@@ -119,17 +224,19 @@ Navigation.events().registerAppLaunchedListener(()=>{
 
 const styles = StyleSheet.create({
   sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+    //marginTop: 32,
+    //paddingHorizontal: 24,
   },
   sectionTitle: {
     fontSize: 24,
     fontWeight: '600',
+    color: '#000'
   },
   sectionDescription: {
-    marginTop: 8,
+    //marginTop: 8,
     fontSize: 18,
     fontWeight: '400',
+    color: '#000'
   },
   highlight: {
     fontWeight: '700',
@@ -138,6 +245,57 @@ const styles = StyleSheet.create({
     width: 50,
     height: 50,
   },
+
+
+  container:{
+    backgroundColor:'#fff',
+    alignItems: 'center',
+  },
+  pad: {
+    paddingHorizontal:10
+  },
+  icon: {
+    width: 75,
+    height: 75,
+  },
+  header: {
+    flexDirection: 'row',
+    paddingTop: 30,
+  },
+  Background: {
+    width: 1200,
+    height: 900,
+    resizeMode: 'stretch',
+    position:'absolute',
+    transform: [{rotateZ: '65deg'}, {rotateY: '50deg'}]
+  },
+
+  boldText: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    color:'#fff',
+  },
+  listText: {
+    width: 350,
+    height: 80,
+    paddingTop: 22,
+    paddingLeft: 50,
+    fontSize: 23,
+    color:'#000',
+  },
+  nextworkButton: { 
+    width: 350,
+    height: 80,
+    backgroundColor:'#EB8C1E',
+    borderRadius: 75,
+  },
+  listArea: {
+    marginTop: 30,
+    width: 415,
+    height: 800,
+    borderRadius: 50,
+    backgroundColor:'#fff',
+  }
 });
 
 Navigation.registerComponent('com.MouseDroid.WelcomeScreen', () => WelcomeScreen);
